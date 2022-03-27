d4bb23a648ac1dcc735f41043d99c1906f2082f6
eryner-z <stfn.bgr@gmail.com>
2022-03-27 16:25:46 +0200

    Create input for first name

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 
 export class Inputs extends Component {
   constructor(props) {
-    super();
+    super(props);
     this.state = {};
   }
 
   render() {
-    return <div className="inputs ">inputs</div>;
+    return (
+      <div className="inputs ">
+        <div className="personal-information-input">
+          <input
+            className="first-name-input"
+            type="text"
+            id="firstNameInput"
+            placeholder="first name"
+            onChange={(e) => {
+              this.props.change(e);
+            }}
+          />
+        </div>
+      </div>
+    );
   }
 }
