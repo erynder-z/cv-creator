1a6f8ad62d7c5abc4dc90d28b7e0622fba811c56
eryner-z <stfn.bgr@gmail.com>
2022-03-27 18:26:46 +0200

    Create grid layout for main components

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -1,35 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 export class Inputs extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
 
-  render() {
-    return (
-      <div className="inputs ">
-        <div className="personal-information-input">
-          <input
-            className="first-name-input"
-            type="text"
-            id="firstNameInput"
-            placeholder="first name"
-            onChange={(e) => {
-              this.props.changeFirstName(e);
-            }}
-          />
-          <input
-            className="last-name-input"
-            type="text"
-            id="lastNameInput"
-            placeholder="last name"
-            onChange={(e) => {
-              this.props.changeLastName(e);
-            }}
-          />
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="inputs ">
+                <div className="personal-information-input">
+                    <div>Personal Infomation</div>
+                    <input
+                        className="first-name-input"
+                        type="text"
+                        id="firstNameInput"
+                        placeholder="first name"
+                        onChange={(e) => {
+                            this.props.changeFirstName(e)
+                        }}
+                    />
+                    <input
+                        className="last-name-input"
+                        type="text"
+                        id="lastNameInput"
+                        placeholder="last name"
+                        onChange={(e) => {
+                            this.props.changeLastName(e)
+                        }}
+                    />
+                </div>
+            </div>
+        )
+    }
 }
--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,18 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 export class Preview extends Component {
-  constructor(props) {
-    super();
-  }
-  render() {
-    const { personal } = this.props;
-    return (
-      <div className="preview ">
-        <div className="personal-information-preview">
-          <div className="first-name-preview">{personal.firstName}</div>
-          <div className="last-name-preview">{personal.lastName}</div>
-        </div>
-      </div>
-    );
-  }
+    constructor(props) {
+        super()
+    }
+    render() {
+        const { personal } = this.props
+        return (
+            <div className="preview ">
+                <div className="personal-information-preview-top">
+                    <div className="first-name-preview">
+                        {personal.firstName}&nbsp;
+                    </div>
+                    <div className="last-name-preview">{personal.lastName}</div>
+                </div>
+
+                <div className="personal-information-preview-side">
+                    Personal Info
+                </div>
+                <div className="info-wrapper-preview">Experience</div>
+            </div>
+        )
+    }
 }
