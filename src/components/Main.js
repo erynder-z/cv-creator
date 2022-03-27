ba6a030df4018214de803dc83fdb2c0cdd4822a3
eryner-z <stfn.bgr@gmail.com>
2022-03-27 18:45:42 +0200

    Add logic to preview/input title

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,54 +1,68 @@
-import React, { Component } from 'react';
-import uniqid from 'uniqid';
-import { Inputs } from './Inputs';
-import { Preview } from './Preview';
+import React, { Component } from 'react'
+import uniqid from 'uniqid'
+import { Inputs } from './Inputs'
+import { Preview } from './Preview'
 
 export class Main extends Component {
-  constructor() {
-    super();
-    this.state = {
-      personal: {
-        id: uniqid(),
-        firstName: '',
-        lastName: '',
-      },
-    };
-  }
+    constructor() {
+        super()
+        this.state = {
+            personal: {
+                id: uniqid(),
+                firstName: '',
+                lastName: '',
+                title: '',
+            },
+        }
+    }
 
-  handleFirstNameInput(e) {
-    this.setState((prevState) => ({
-      personal: {
-        ...prevState.personal,
-        firstName: e.target.value,
-        id: this.state.personal.id,
-      },
-    }));
-  }
+    handleFirstNameInput(e) {
+        this.setState((prevState) => ({
+            personal: {
+                ...prevState.personal,
+                firstName: e.target.value,
+                id: this.state.personal.id,
+            },
+        }))
+    }
 
-  handleLastNameInput(e) {
-    this.setState((prevState) => ({
-      personal: {
-        ...prevState.personal,
-        lastName: e.target.value,
-        id: this.state.personal.id,
-      },
-    }));
-  }
+    handleLastNameInput(e) {
+        this.setState((prevState) => ({
+            personal: {
+                ...prevState.personal,
+                lastName: e.target.value,
+                id: this.state.personal.id,
+            },
+        }))
+    }
 
-  render() {
-    const personal = this.state.personal;
-    return (
-      <div className="main-wrapper">
-        <Inputs
-          changeFirstName={(e) => {
-            this.handleFirstNameInput(e);
-          }}
-          changeLastName={(e) => {
-            this.handleLastNameInput(e);
-          }}
-        ></Inputs>
-        <Preview personal={personal}></Preview>
-      </div>
-    );
-  }
+    handleTitleInput(e) {
+        this.setState((prevState) => ({
+            personal: {
+                ...prevState.personal,
+                title: e.target.value,
+                id: this.state.personal.id,
+            },
+        }))
+    }
+
+    render() {
+        const personal = this.state.personal
+        return (
+            <div className="main-wrapper">
+                <Inputs
+                    changeFirstName={(e) => {
+                        this.handleFirstNameInput(e)
+                    }}
+                    changeLastName={(e) => {
+                        this.handleLastNameInput(e)
+                    }}
+                    changeTitle={(e) => {
+                        this.handleTitleInput(e)
+                    }}
+                ></Inputs>
+                <Preview personal={personal}></Preview>
+            </div>
+        )
+    }
 }
