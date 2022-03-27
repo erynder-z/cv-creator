23efb7cb0b42d659d0f73103f26c863cca46d931
eryner-z <stfn.bgr@gmail.com>
2022-03-27 20:01:04 +0200

    Add preview for address

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -17,7 +17,9 @@ export class Preview extends Component {
 
                 <div className="personal-information-preview-side">
                     Personal Info
+                    <div className="title-preview">{personal.address}</div>
                 </div>
+
                 <div className="info-wrapper-preview">
                     <div className="title-preview">{personal.title}</div>
                 </div>
