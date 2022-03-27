81a05363673c7eb20d0a18f53a256d57afd56150
eryner-z <stfn.bgr@gmail.com>
2022-03-27 17:30:31 +0200

    Add preview for last name

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -10,6 +10,7 @@ export class Preview extends Component {
       <div className="preview ">
         <div className="personal-information-preview">
           <div className="first-name-preview">{personal.firstName}</div>
+          <div className="last-name-preview">{personal.lastName}</div>
         </div>
       </div>
     );
