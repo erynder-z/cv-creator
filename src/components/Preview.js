9ca171e9e119f438f0d453eb3da539885bb5bc5e
eryner-z <stfn.bgr@gmail.com>
2022-03-27 21:03:03 +0200

    Add a placeholder image

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import placeholder from '../assets/placeholder.png'
 
 export class Preview extends Component {
     constructor(props) {
@@ -16,6 +17,9 @@ export class Preview extends Component {
                 </div>
 
                 <div className="personal-information-preview-side">
+                    <div className="picture">
+                        <img src={placeholder} alt="" />
+                    </div>
                     <h4>Personal Info</h4>
                     <div className="title-preview">
                         <h5>Address:</h5>
