96a3a33d00df3862b4ddfaf6730a332a13ae9c2c
eryner-z <stfn.bgr@gmail.com>
2022-03-28 22:16:40 +0200

    Remove trailing commas

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React, { Component } from 'react';
-import './styles/App.css';
-import './styles/Header.css';
-import './styles/Main.css';
-import './styles/Footer.css';
-import { Header } from './components/Header';
-import { Main } from './components/Main';
-import { Footer } from './components/Footer';
+import React, { Component } from 'react'
+import './styles/App.css'
+import './styles/Header.css'
+import './styles/Main.css'
+import './styles/Footer.css'
+import { Header } from './components/Header'
+import { Main } from './components/Main'
+import { Footer } from './components/Footer'
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
+    constructor() {
+        super()
+        this.state = {}
+    }
 
-  render() {
-    return (
-      <div>
-        <Header></Header>
-        <Main></Main>
-        <Footer></Footer>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Header></Header>
+                <Main></Main>
+                <Footer></Footer>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
