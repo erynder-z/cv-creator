bf32ebc91a742e1165f6127976cc604b272a05ed
eryner-z <stfn.bgr@gmail.com>
2022-03-28 18:23:52 +0200

    Remove unused props

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 export class Inputs extends Component {
-    constructor(props) {
+    constructor() {
         super()
         this.state = {}
     }
