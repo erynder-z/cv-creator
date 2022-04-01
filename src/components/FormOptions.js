a7d2c06d48944d47268195e60dc8c790a85cd440
eryner-z <stfn.bgr@gmail.com>
2022-04-01 20:05:42 +0200

    Create button to add a sample cv

---
+++ src/components/FormOptions.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export class FormOptions extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div className="options ">
+                <div className="options-input">
+                    <div>Document options</div>
+                    <div
+                        className={'sample-button'}
+                        onClick={() => {
+                            this.props.getSample()
+                        }}
+                    >
+                        Load Sample
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+FormOptions.propTypes = {
+    getSample: PropTypes.func,
+}
