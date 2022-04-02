8ce34e239da5be8b1c808a7c11de1f980f0555bc
eryner-z <stfn.bgr@gmail.com>
2022-04-02 19:37:05 +0200

    Add classes to divs

--- src/components/PersonalInfoSidePreview.js
+++ src/components/PersonalInfoSidePreview.js
@@ -16,19 +16,19 @@ export class PersonalInfoSidePreview extends Component {
                 <h4>Personal Info</h4>
                 <div className="personal-preview">
                     <h5>Date of birth:</h5>
-                    {personal.birthday}
+                    <div className="preview-item">{personal.birthday}</div>
                 </div>
                 <div className="personal-preview">
                     <h5>Address:</h5>
-                    {personal.address}
+                    <div className="preview-item">{personal.address}</div>
                 </div>
                 <div className="personal-preview">
                     <h5>Phone:</h5>
-                    {personal.phone}
+                    <div className="preview-item">{personal.phone}</div>
                 </div>
                 <div className="personal-preview">
                     <h5>Email:</h5>
-                    {personal.email}
+                    <div className="preview-item">{personal.email}</div>
                 </div>
             </div>
         )
