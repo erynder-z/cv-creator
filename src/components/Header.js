acba6725392adfd7d9237088fb995a461711afa0
eryner-z <stfn.bgr@gmail.com>
2022-04-03 18:08:30 +0200

    Add simple header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 export class Header extends Component {
-  constructor(props) {
-    super();
-    this.state = {};
-  }
+    constructor() {
+        super()
+        this.state = {}
+    }
 
-  render() {
-    return <nav className="navbar ">header</nav>;
-  }
+    render() {
+        return (
+            <nav className="navbar ">
+                <div className="header-text">CV-CREATOR</div>
+            </nav>
+        )
+    }
 }
