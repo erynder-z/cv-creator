ce70d5aee14854be5f61825ba1b12e6e400d47ff
erynder-z <stfn.bgr@gmail.com>
2022-04-10 18:18:17 +0200

    Refactor to functional component

--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -1,120 +1,113 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
-export class PersonalInfo extends Component {
-    constructor() {
-        super()
-        this.state = {}
-    }
+export const PersonalInfo = (props) => {
+    const { personal } = props
+    return (
+        <div className="personal-information-input">
+            <div className="input-section-title">Personal Infomation</div>
+            <input
+                className="first-name-input"
+                type="text"
+                id="firstNameInput"
+                placeholder="first name"
+                value={personal.firstName}
+                onChange={(e) => {
+                    props.changeFirstName(e)
+                }}
+            />
+            <input
+                className="last-name-input"
+                type="text"
+                id="lastNameInput"
+                placeholder="last name"
+                value={personal.lastName}
+                onChange={(e) => {
+                    props.changeLastName(e)
+                }}
+            />
+            <input
+                className="birthday-input"
+                type="text"
+                id="birthdayInput"
+                placeholder="birthday"
+                value={personal.birthday}
+                onFocus={(e) => {
+                    props.changeInputTypeIn(e)
+                }}
+                onBlur={(e) => {
+                    props.changeInputTypeOut(e)
+                }}
+                onChange={(e) => {
+                    props.changeDOB(e)
+                }}
+            />
 
-    render() {
-        const { personal } = this.props
-        return (
-            <div className="personal-information-input">
-                <div className="input-section-title">Personal Infomation</div>
-                <input
-                    className="first-name-input"
-                    type="text"
-                    id="firstNameInput"
-                    placeholder="first name"
-                    value={personal.firstName}
-                    onChange={(e) => {
-                        this.props.changeFirstName(e)
-                    }}
-                />
-                <input
-                    className="last-name-input"
-                    type="text"
-                    id="lastNameInput"
-                    placeholder="last name"
-                    value={personal.lastName}
-                    onChange={(e) => {
-                        this.props.changeLastName(e)
-                    }}
-                />
-                <input
-                    className="birthday-input"
-                    type="text"
-                    id="birthdayInput"
-                    placeholder="birthday"
-                    value={personal.birthday}
-                    onFocus={(e) => {
-                        this.props.changeInputTypeIn(e)
-                    }}
-                    onBlur={(e) => {
-                        this.props.changeInputTypeOut(e)
-                    }}
-                    onChange={(e) => {
-                        this.props.changeDOB(e)
-                    }}
-                />
-
-                <input
-                    className="address-input"
-                    type="text"
-                    id="addressInput"
-                    placeholder="address"
-                    value={personal.address}
-                    onChange={(e) => {
-                        this.props.changeAddress(e)
-                    }}
-                />
-                <input
-                    className="phone-input"
-                    type="phone"
-                    id="phoneInput"
-                    placeholder="phone"
-                    value={personal.phone}
-                    onChange={(e) => {
-                        this.props.changePhone(e)
-                    }}
-                />
-                <input
-                    className="email-input"
-                    type="email"
-                    id="emailInput"
-                    placeholder="email"
-                    value={personal.email}
-                    onChange={(e) => {
-                        this.props.changeEmail(e)
-                    }}
-                />
-                <input
-                    className="title-input"
-                    type="text"
-                    id="titleInput"
-                    placeholder="title"
-                    value={personal.title}
-                    onChange={(e) => {
-                        this.props.changeTitle(e)
-                    }}
-                />
-                <textarea
-                    className="description-input"
-                    type="text"
-                    id="descriptionInput"
-                    placeholder="description"
-                    value={personal.description}
-                    onChange={(e) => {
-                        this.props.changeDescription(e)
-                    }}
-                />
-                <label htmlFor="picture" className="custom-file-upload-label">
-                    Change picture
-                </label>
-                <input
-                    className="custom-file-upload"
-                    type="file"
-                    id="picture"
-                    name="picture"
-                    accept="image/png, image/jpeg"
-                    onChange={(e) => {
-                        this.props.uploadPic(e)
-                    }}
-                ></input>
-            </div>
-        )
-    }
+            <input
+                className="address-input"
+                type="text"
+                id="addressInput"
+                placeholder="address"
+                value={personal.address}
+                onChange={(e) => {
+                    props.changeAddress(e)
+                }}
+            />
+            <input
+                className="phone-input"
+                type="phone"
+                id="phoneInput"
+                placeholder="phone"
+                value={personal.phone}
+                onChange={(e) => {
+                    props.changePhone(e)
+                }}
+            />
+            <input
+                className="email-input"
+                type="email"
+                id="emailInput"
+                placeholder="email"
+                value={personal.email}
+                onChange={(e) => {
+                    props.changeEmail(e)
+                }}
+            />
+            <input
+                className="title-input"
+                type="text"
+                id="titleInput"
+                placeholder="title"
+                value={personal.title}
+                onChange={(e) => {
+                    props.changeTitle(e)
+                }}
+            />
+            <textarea
+                className="description-input"
+                type="text"
+                id="descriptionInput"
+                placeholder="description"
+                value={personal.description}
+                onChange={(e) => {
+                    props.changeDescription(e)
+                }}
+            />
+            <label htmlFor="picture" className="custom-file-upload-label">
+                Change picture
+            </label>
+            <input
+                className="custom-file-upload"
+                type="file"
+                id="picture"
+                name="picture"
+                accept="image/png, image/jpeg"
+                onChange={(e) => {
+                    props.uploadPic(e)
+                }}
+            ></input>
+        </div>
+    )
 }
 
 PersonalInfo.propTypes = {
