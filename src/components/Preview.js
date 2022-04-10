e9a12f136313b861e660d24c270758cb326a224a
erynder-z <stfn.bgr@gmail.com>
2022-04-10 20:14:38 +0200

    Refactor to functional component

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,78 +1,69 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { PersonalInfoSidePreview } from './PersonalInfoSidePreview'
 import { WorkExperiencePreview } from './WorkExperiencePreview'
 import { EducationPreview } from './EducationPreview'
 import { SkillsPreview } from './SkillsPreview'
 
-export class Preview extends Component {
-    constructor(props) {
-        super(props)
-    }
-    render() {
-        const { personal } = this.props
+export const Preview = React.forwardRef((props, ref) => {
+    const { personal } = props
 
-        return (
-            <div className="preview ">
-                <div className="personal-information-preview-top">
-                    <div className="first-name-preview">
-                        {personal.firstName}&nbsp;
-                    </div>
-                    <div className="last-name-preview">{personal.lastName}</div>
+    return (
+        <div ref={ref} className="preview ">
+            <div className="personal-information-preview-top">
+                <div className="first-name-preview">
+                    {personal.firstName}&nbsp;
+                </div>
+                <div className="last-name-preview">{personal.lastName}</div>
+            </div>
+
+            <PersonalInfoSidePreview {...props}></PersonalInfoSidePreview>
+
+            <div className="info-preview">
+                <div className="personal-preview-title">{personal.title}</div>
+                <h5>About me:</h5>
+                <div className="personal-preview">{personal.description}</div>
+
+                <div className="work-preview">
+                    <WorkExperiencePreview
+                        key={props.experience.id}
+                        id={props.experience.id}
+                        position={props.experience.position}
+                        company={props.experience.company}
+                        from={props.experience.from}
+                        to={props.experience.to}
+                        {...props}
+                    ></WorkExperiencePreview>
                 </div>
 
-                <PersonalInfoSidePreview
-                    {...this.props}
-                ></PersonalInfoSidePreview>
+                <div className="edu-preview">
+                    <EducationPreview
+                        key={props.education.id}
+                        id={props.education.id}
+                        institution={props.education.institution}
+                        city={props.education.city}
+                        degree={props.education.degree}
+                        major={props.education.major}
+                        from={props.education.from}
+                        to={props.education.to}
+                        {...props}
+                    ></EducationPreview>
+                </div>
 
-                <div className="info-preview">
-                    <div className="personal-preview-title">
-                        {personal.title}
-                    </div>
-                    <h5>About me:</h5>
-                    <div className="personal-preview">
-                        {personal.description}
-                    </div>
-
-                    <div className="work-preview">
-                        <WorkExperiencePreview
-                            key={this.props.experience.id}
-                            id={this.props.experience.id}
-                            position={this.props.experience.position}
-                            company={this.props.experience.company}
-                            from={this.props.experience.from}
-                            to={this.props.experience.to}
-                            {...this.props}
-                        ></WorkExperiencePreview>
-                    </div>
-
-                    <div className="edu-preview">
-                        <EducationPreview
-                            key={this.props.education.id}
-                            id={this.props.education.id}
-                            institution={this.props.education.institution}
-                            city={this.props.education.city}
-                            degree={this.props.education.degree}
-                            major={this.props.education.major}
-                            from={this.props.education.from}
-                            to={this.props.education.to}
-                            {...this.props}
-                        ></EducationPreview>
-                    </div>
-
-                    <div className="skills-preview">
-                        <SkillsPreview
-                            key={this.props.skill.id}
-                            id={this.props.skill.id}
-                            name={this.props.skill.name}
-                            {...this.props}
-                        ></SkillsPreview>
-                    </div>
+                <div className="skills-preview">
+                    <SkillsPreview
+                        key={props.skill.id}
+                        id={props.skill.id}
+                        name={props.skill.name}
+                        {...props}
+                    ></SkillsPreview>
                 </div>
             </div>
-        )
-    }
-}
+        </div>
+    )
+})
+
+Preview.displayName = 'Preview'
 
 Preview.propTypes = {
     personal: PropTypes.object,
