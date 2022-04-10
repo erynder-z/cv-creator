fce2af099ecc61545c884754797f967a4070f1fc
erynder-z <stfn.bgr@gmail.com>
2022-04-10 20:04:03 +0200

    Refactor to functinal component

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,123 +1,117 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
-export class Education extends Component {
-    constructor() {
-        super()
-        this.state = {}
+export const Education = (props) => {
+    const id = props.id
+    let sectionItem
+    if (props.sectionValues === undefined) {
+        sectionItem = props.education
+    } else {
+        sectionItem = props.sectionValues
     }
 
-    render() {
-        const id = this._reactInternals.key
-        let sectionItem
-        if (this.props.sectionValues === undefined) {
-            sectionItem = this.props.education
-        } else {
-            sectionItem = this.props.sectionValues
-        }
+    const isHidden = props.isHidden
+    const bottomItem = props.bottomItem
 
-        const isHidden = this.props.isHidden
-        const bottomItem = this.props.bottomItem
-
-        return (
-            <div className="inputs ">
-                <div className="education-input">
-                    <div className="input-section-title">Education</div>
-                    <input
-                        className="education-institution-input"
-                        type="text"
-                        id="institutionInput"
-                        placeholder="institution"
-                        value={sectionItem.institution}
-                        onChange={(e) => {
-                            this.props.changeInstitution(e, id)
-                        }}
-                    />
-                    <input
-                        className="education-city-input"
-                        type="text"
-                        id="eduCityInput"
-                        placeholder="city"
-                        value={sectionItem.city}
-                        onChange={(e) => {
-                            this.props.changeCity(e, id)
-                        }}
-                    />
-                    <input
-                        className="education-degree-input"
-                        type="text"
-                        id="eduDegreeInput"
-                        placeholder="degree"
-                        value={sectionItem.degree}
-                        onChange={(e) => {
-                            this.props.changeDegree(e, id)
-                        }}
-                    />
-                    <input
-                        className="education-major-input"
-                        type="text"
-                        id="eduMajorInput"
-                        placeholder="major"
-                        value={sectionItem.major}
-                        onChange={(e) => {
-                            this.props.changeMajor(e, id)
-                        }}
-                    />
-                    <input
-                        className="education-from-input"
-                        type="text"
-                        id="eduFromInput"
-                        placeholder="from"
-                        value={sectionItem.from}
-                        onFocus={(e) => {
-                            this.props.changeInputTypeIn(e, id)
-                        }}
-                        onBlur={(e) => {
-                            this.props.changeInputTypeOut(e, id)
-                        }}
-                        onChange={(e) => {
-                            this.props.changeEducationFrom(e, id)
-                        }}
-                    />
-                    <input
-                        className="education-to-input"
-                        type="text"
-                        id="eduToInput"
-                        placeholder="to"
-                        value={sectionItem.to}
-                        onFocus={(e) => {
-                            this.props.changeInputTypeIn(e, id)
-                        }}
-                        onBlur={(e) => {
-                            this.props.changeInputTypeOut(e, id)
-                        }}
-                        onChange={(e) => {
-                            this.props.changeEducationTo(e, id)
-                        }}
-                    />
-                    <div
-                        className={isHidden ? 'hidden' : 'addSection'}
-                        onClick={(e) => {
-                            this.props.addInputFieldEducation(e, id)
-                        }}
-                    >
-                        Add education section
-                    </div>
-                    <div
-                        className={bottomItem ? 'hidden' : 'removeSection'}
-                        onClick={(e) => {
-                            this.props.removeInputFieldEducation(e, id)
-                        }}
-                    >
-                        Remove this section
-                    </div>
+    return (
+        <div className="inputs ">
+            <div className="education-input">
+                <div className="input-section-title">Education</div>
+                <input
+                    className="education-institution-input"
+                    type="text"
+                    id="institutionInput"
+                    placeholder="institution"
+                    value={sectionItem.institution}
+                    onChange={(e) => {
+                        props.changeInstitution(e, id)
+                    }}
+                />
+                <input
+                    className="education-city-input"
+                    type="text"
+                    id="eduCityInput"
+                    placeholder="city"
+                    value={sectionItem.city}
+                    onChange={(e) => {
+                        props.changeCity(e, id)
+                    }}
+                />
+                <input
+                    className="education-degree-input"
+                    type="text"
+                    id="eduDegreeInput"
+                    placeholder="degree"
+                    value={sectionItem.degree}
+                    onChange={(e) => {
+                        props.changeDegree(e, id)
+                    }}
+                />
+                <input
+                    className="education-major-input"
+                    type="text"
+                    id="eduMajorInput"
+                    placeholder="major"
+                    value={sectionItem.major}
+                    onChange={(e) => {
+                        props.changeMajor(e, id)
+                    }}
+                />
+                <input
+                    className="education-from-input"
+                    type="text"
+                    id="eduFromInput"
+                    placeholder="from"
+                    value={sectionItem.from}
+                    onFocus={(e) => {
+                        props.changeInputTypeIn(e, id)
+                    }}
+                    onBlur={(e) => {
+                        props.changeInputTypeOut(e, id)
+                    }}
+                    onChange={(e) => {
+                        props.changeEducationFrom(e, id)
+                    }}
+                />
+                <input
+                    className="education-to-input"
+                    type="text"
+                    id="eduToInput"
+                    placeholder="to"
+                    value={sectionItem.to}
+                    onFocus={(e) => {
+                        props.changeInputTypeIn(e, id)
+                    }}
+                    onBlur={(e) => {
+                        props.changeInputTypeOut(e, id)
+                    }}
+                    onChange={(e) => {
+                        props.changeEducationTo(e, id)
+                    }}
+                />
+                <div
+                    className={isHidden ? 'hidden' : 'addSection'}
+                    onClick={(e) => {
+                        props.addInputFieldEducation(e, id)
+                    }}
+                >
+                    Add education section
+                </div>
+                <div
+                    className={bottomItem ? 'hidden' : 'removeSection'}
+                    onClick={(e) => {
+                        props.removeInputFieldEducation(e, id)
+                    }}
+                >
+                    Remove this section
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 Education.propTypes = {
+    id: PropTypes.string,
     changeInstitution: PropTypes.func,
     changeCity: PropTypes.func,
     changeDegree: PropTypes.func,
